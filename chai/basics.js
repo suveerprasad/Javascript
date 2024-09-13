b3e02209b33b16252319b7d8d58221f8432f1095
suveerbabu <suveerprasad100@gmail.com>
2024-09-13 16:03:44 +0530

    kk

--- chai/basics.js
+++ chai/basics.js
@@ -7,6 +7,14 @@ function addtwonum(num1,num2){
     return num1 + num2;
 }
 
-let value = addtwonum(1,"2");
-console.log(value);
+// let value = addtwonum(1,"2");
+// console.log(value);
 
+function loginusermessage(username){
+    if(!username){
+        return "Invalid";
+    }
+    return `${username} just logged in`;
+}
+
+console.log(loginusermessage());
