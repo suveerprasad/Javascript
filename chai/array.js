8be45dd7404abd36e1666b723dbfbb5b447fd975
suveerbabu <suveerprasad100@gmail.com>
2024-09-13 15:49:24 +0530

    hi

---
+++ chai/array.js
@@ -0,0 +1,59 @@
+// const myarr = [0,1,2,3];
+// // console.log(myarr[0]);
+
+// const arr = new Array(1,2,3,4);
+// console.log(arr);
+
+// myarr.push(1);
+// myarr.push(2);
+// myarr.pop();
+// console.log(myarr);
+// myarr.unshift(9);
+// console.log(myarr);
+// myarr.shift();
+// console.log(myarr);
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(3));
+
+// const arr1 = myarr.join();
+// console.log(arr1); /// typeof String
+
+// console.log(myarr);
+// const arr2 = myarr.slice(1,3);
+// console.log(`Slice : `,arr2);
+
+// console.log(myarr);
+// const arr3 = myarr.splice(1,3);
+// console.log(`Splice : `,arr3);
+// console.log(myarr);
+
+// const heros = ["sai","vijay","sagar"];
+// const ep = ["hanmant"];
+
+// heros.push(ep);
+// console.log(heros);
+
+// const arr = heros.concat(ep);
+// console.log(arr);
+
+// const allheros = [...heros,...ep];
+// console.log(allheros);
+
+// const arr1 = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+
+// const arr2 = arr1.flat(Infinity);
+// console.log(arr2);
+
+console.log(Array.isArray("Sai"));
+console.log(Array.from("Suveer"));
+console.log(Array.from({name : "Suveer"}));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1, score2,score3));
+
+
+
+
