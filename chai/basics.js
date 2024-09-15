c205ca3932595d836414e23c0d5d095c8ca5c3ea
suveerbabu <suveerprasad100@gmail.com>
2024-09-15 13:31:29 +0530

    kkk

--- chai/basics.js
+++ chai/basics.js
@@ -17,4 +17,100 @@ function loginusermessage(username){
     return `${username} just logged in`;
 }
 
-console.log(loginusermessage());
+// console.log(loginusermessage());
+
+function calculateprice(val1,val2,...num1){
+    return num1;
+}
+
+// console.log(calculateprice(200,400,500,900));
+
+const user = {
+    username : "Suveer",
+    price : 199
+}
+
+function handleobj(obj){
+    console.log(`Username : ${obj.username}`);
+}
+
+// handleobj({
+//     username : "Sagar"
+// })
+
+// handleobj(user);
+
+// let c = 300;
+//  if(true){
+//     let c = 30;
+//     console.log(c);
+//  }
+//  console.log(c);
+
+// function one(){
+//     const username = "Suveer"
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     two();
+//     // console.log(website);
+// }
+
+// one();
+
+// console.log(addone(5));
+// function addone(num){
+//     return num + 1;
+// }
+
+// addtwo(5);
+
+// const addtwo = function(num){
+//     return num + 2;
+// }
+
+const user1 = {
+    username : "Suveer",
+    price : 999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username} logged in`)
+        console.log(this);
+    }
+}
+
+// user1.welcomeMessage();
+// user1.username = "Vijay";
+// user1.welcomeMessage();
+
+// function chai(){
+//     console.log(this);
+// }
+
+// chai();
+
+// const chai = () => {
+//     let username = "Suveer"
+//     console.log(this);
+// }
+// chai();
+
+// const addtwo = (num1,num2) => {
+//     return num1 + num2;
+// }
+
+// const addtwo = (num1,num2) => (num1 + num2)
+// console.log(addtwo(2,3));
+
+
+// {function chai(){
+//     console.log('DB Connected');
+// }}{}
+
+//javascript execution context
+
+//global execution context
+
+
+
